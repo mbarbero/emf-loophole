80dae2868d881c735ed4229ce50447cd291477af
Mikaël Barbero <mikael.barbero@gmail.com>
2013-05-14 19:04:34 +0200

    initial commit

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VERSIONS=('2.5' '2.6' '2.7' '2.8' '2.9');
+
+TIMESTAMP=`ls packaging/org.eclipselabs.emf.loophole-repository/2.5/target/repository/plugins/org.eclipselabs.emf.loophole_* \
+		| grep -v '_.*_.*' | cut -d'/' -f7 | cut -d'.' -f7`
+
+for v in ${VERSIONS[@]}; do
+	echo "Copying build" ${v}"-"${TIMESTAMP}
+	mkdir -p ${v}/${TIMESTAMP}/
+	cp -rf packaging/org.eclipselabs.emf.loophole-repository/${v}/target/repository/* ${v}/${TIMESTAMP}/
+	
+	./comp-repo.sh ${v} \
+		--name "EMF Loophole releases repository for EMF ${v}" \
+		--eclipse '/Applications/eclipse 3.8.2' \
+		add ${v}/${TIMESTAMP}/
+
+	thisFolder=`pwd`
+	./comp-repo.sh ${thisFolder} \
+		--name "EMF Loophole releases repository for EMF" \
+		--eclipse '/Applications/eclipse 3.8.2' \
+		add ${v}/${TIMESTAMP}/
+done
+
+git add 2.*
+git add composite*.jar
+git commit -m "added build ${TIMESTAMP}"
---
+++ test.sh
@@ -0,0 +1,6 @@
+if [ ! -d $1 ]; then
+	echo "nok"
+else
+	echo "ok"
+fi
+
